f132c06c77ce74af183cc6ce4dbaff4a5470ed93
Subham <growlyxdev@gmail.com>
2023-06-13 08:53:26 -0400

    feat: add aggregate builders for LeaderboardType

--- src/main/kotlin/gg/tropic/souppvp/leaderboard/LeaderboardType.kt
+++ src/main/kotlin/gg/tropic/souppvp/leaderboard/LeaderboardType.kt
@@ -1,17 +1,45 @@
 package gg.tropic.souppvp.leaderboard
 
+import org.bson.Document
+
 /**
  * @author GrowlyX
  * @since 6/13/2023
  */
+fun aggregateFieldSortedDescending(field: String) =
+    listOf(
+        Document(
+            "\$sort",
+            Document(field, -1L)
+        ),
+        Document("\$limit", 10L),
+        Document(
+            "\$project",
+            Document("_id", "\$_id")
+                .append(
+                    "value",
+                    "\$$field"
+                )
+        )
+    )
+
 enum class LeaderboardType(
-    val field: String
+    val field: String,
+    val aggregate: () -> List<Document>
 )
 {
-    Kills("kills"),
-    Deaths("deaths"),
-    Balances("coins"),
-    Consumed_Soup("soupsConsumed");
+    Kills("kills") {
+        aggregateFieldSortedDescending(field)
+    },
+    Deaths("deaths") {
+        aggregateFieldSortedDescending(field)
+    },
+    Balances("coins") {
+        aggregateFieldSortedDescending(field)
+    },
+    Consumed_Soup("soupsConsumed") {
+        aggregateFieldSortedDescending(field)
+    };
 
     /**
      * This is sorta ghetto but too lazy right now
